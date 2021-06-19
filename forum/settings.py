726e16a0fb838ba91d73b1adf600d0d71502400e
Ives Costa <ivespauiniam@gmail.com>
2021-06-19 16:40:50 -0500

    ...

--- forum/settings.py
+++ forum/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/' 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
