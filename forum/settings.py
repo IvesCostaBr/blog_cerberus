98f3d7c6124b8dbce55fa8ed16eefe5a89d22e7a
Ives Costa <ivespauiniam@gmail.com>
2021-06-19 16:57:08 -0500

    stati files fixed try

--- forum/settings.py
+++ forum/settings.py
@@ -111,12 +111,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/' 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 
 MEDIA_URL = '/media/'
 
